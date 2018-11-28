89ccc73f9470b7a74fefa0afcb73647347938ab3
Stefano Sgorlon <stefano.sgorlon@digital.hmrc.gov.uk>
2018-11-28 21:02:32 +0000

    BAU: update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 object AppDependencies {
 
   lazy val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-play-25" % "3.15.0",
+    "uk.gov.hmrc" %% "bootstrap-play-25" % "4.0.0",
     "uk.gov.hmrc" %% "play-json-union-formatter" % "1.4.0",
     "uk.gov.hmrc" %% "simple-reactivemongo" % "7.3.0-play-25",
     ws
@@ -13,12 +13,12 @@ object AppDependencies {
   lazy val scope: String = "test,it"
 
   lazy val test = Seq(
-    "org.mockito" % "mockito-core" % "2.23.0" % scope,
+    "org.mockito" % "mockito-core" % "2.23.4" % scope,
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
     "org.scalaj" %% "scalaj-http" % "2.4.1" % scope,
     "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1" % scope,
     "uk.gov.hmrc" %% "hmrctest" % "3.2.0" % scope,
-    "uk.gov.hmrc" %% "reactivemongo-test" % "4.1.0-play-25" % scope
+    "uk.gov.hmrc" %% "reactivemongo-test" % "4.2.0-play-25" % scope
   )
 
 }
