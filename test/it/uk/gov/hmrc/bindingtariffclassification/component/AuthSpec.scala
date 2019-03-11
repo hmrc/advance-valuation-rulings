7c32a88960afee032d217dffdc566bb31c1a31d1
Stefano Sgorlon <stefano.sgorlon@digital.hmrc.gov.uk>
2019-03-11 15:47:00 +0000

    BAU: using unique port in the integration tests

--- test/it/uk/gov/hmrc/bindingtariffclassification/component/AuthSpec.scala
+++ test/it/uk/gov/hmrc/bindingtariffclassification/component/AuthSpec.scala
@@ -22,7 +22,7 @@ import scalaj.http.Http
 
 class AuthSpec extends BaseFeatureSpec {
 
-  override lazy val port = 14682
+  override lazy val port = 14684
   protected val serviceUrl = s"http://localhost:$port"
 
   feature("Authentication") {
