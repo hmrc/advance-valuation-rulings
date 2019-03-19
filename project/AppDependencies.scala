d57899dd823f60c9c5968c3349a68a1041cfd237
Stefano Sgorlon <gokyo@users.noreply.github.com>
2019-03-19 14:19:50 +0000

    Update AppDependencies.scala

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -15,8 +15,8 @@ object AppDependencies {
   lazy val scope: String = "test,it"
 
   lazy val test = Seq(
-    "com.github.tomakehurst" %  "wiremock"            % "2.21.0"        % scope,
-    "org.mockito"            %  "mockito-core"        % "2.25.0"        % scope,
+    "com.github.tomakehurst" %  "wiremock"            % "2.22.0"        % scope,
+    "org.mockito"            %  "mockito-core"        % "2.25.1"        % scope,
     "org.pegdown"            %  "pegdown"             % "1.6.0"         % scope,
     "org.scalaj"             %% "scalaj-http"         % "2.4.1"         % scope,
     "org.scalatestplus.play" %% "scalatestplus-play"  % "2.0.1"         % scope,
