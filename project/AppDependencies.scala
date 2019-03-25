66fc15b99fe767e62d7cff5bda8ad62a2c797eac
Stefano Sgorlon <gokyo@users.noreply.github.com>
2019-03-25 17:24:24 +0000

    Update AppDependencies.scala

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
     "uk.gov.hmrc" %% "bootstrap-play-25"          % "4.9.0",
     "uk.gov.hmrc" %% "crypto"                     % "5.3.0",
     "uk.gov.hmrc" %% "play-json-union-formatter"  % "1.5.0",
-    "uk.gov.hmrc" %% "simple-reactivemongo"       % "7.16.0-play-25",
+    "uk.gov.hmrc" %% "simple-reactivemongo"       % "7.14.0-play-25",
     "io.megl"     %% "play-json-extra"            % "2.4.3",
     ws
   )
@@ -15,13 +15,13 @@ object AppDependencies {
   lazy val scope: String = "test,it"
 
   lazy val test = Seq(
-    "com.github.tomakehurst" %  "wiremock"            % "2.22.0"         % scope,
-    "org.mockito"            %  "mockito-core"        % "2.25.1"         % scope,
-    "org.pegdown"            %  "pegdown"             % "1.6.0"          % scope,
-    "org.scalaj"             %% "scalaj-http"         % "2.4.1"          % scope,
-    "org.scalatestplus.play" %% "scalatestplus-play"  % "2.0.1"          % scope,
-    "uk.gov.hmrc"            %% "hmrctest"            % "3.6.0-play-25"  % scope,
-    "uk.gov.hmrc"            %% "reactivemongo-test"  % "4.10.0-play-25" % scope
+    "com.github.tomakehurst" %  "wiremock"            % "2.22.0"        % scope,
+    "org.mockito"            %  "mockito-core"        % "2.25.1"        % scope,
+    "org.pegdown"            %  "pegdown"             % "1.6.0"         % scope,
+    "org.scalaj"             %% "scalaj-http"         % "2.4.1"         % scope,
+    "org.scalatestplus.play" %% "scalatestplus-play"  % "2.0.1"         % scope,
+    "uk.gov.hmrc"            %% "hmrctest"            % "3.6.0-play-25" % scope,
+    "uk.gov.hmrc"            %% "reactivemongo-test"  % "4.9.0-play-25" % scope
   )
 
 }
