a6e25531a6897d45f2d743c40b6897c3c028e85f
Ed Wilson <4864094+ed0906@users.noreply.github.com>
2019-06-03 10:29:28 +0100

    DIT - Dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,10 +4,10 @@ import sbt._
 object AppDependencies {
 
   lazy val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-play-25"          % "4.11.0",
+    "uk.gov.hmrc" %% "bootstrap-play-25"          % "4.12.0",
     "uk.gov.hmrc" %% "crypto"                     % "5.3.0",
     "uk.gov.hmrc" %% "play-json-union-formatter"  % "1.5.0",
-    "uk.gov.hmrc" %% "simple-reactivemongo"       % "7.14.0-play-25",
+    "uk.gov.hmrc" %% "simple-reactivemongo"       % "7.19.0-play-25",
     "io.megl"     %% "play-json-extra"            % "2.4.3",
     ws
   )
@@ -21,7 +21,7 @@ object AppDependencies {
     "org.scalaj"             %% "scalaj-http"         % "2.4.1"         % scope,
     "org.scalatestplus.play" %% "scalatestplus-play"  % "2.0.1"         % scope,
     "uk.gov.hmrc"            %% "hmrctest"            % "3.8.0-play-25" % scope,
-    "uk.gov.hmrc"            %% "reactivemongo-test"  % "4.13.0-play-25" % scope
+    "uk.gov.hmrc"            %% "reactivemongo-test"  % "4.14.0-play-25" % scope
   )
 
 }
