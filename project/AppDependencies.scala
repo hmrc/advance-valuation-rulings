9e3e835525bca3465854ff140f6a54a42d5f2a9a
joe@notmyemail.com <joe@notmyemail.com>
2019-09-30 11:46:18 +0100

    upgraded reactive mongo

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
     "uk.gov.hmrc" %% "bootstrap-play-25"          % "4.12.0",
     "uk.gov.hmrc" %% "crypto"                     % "5.3.0",
     "uk.gov.hmrc" %% "play-json-union-formatter"  % "1.5.0",
-    "uk.gov.hmrc" %% "simple-reactivemongo"       % "7.19.0-play-25",
+    "uk.gov.hmrc" %% "simple-reactivemongo"       % "7.20.0-play-25",
     "io.megl"     %% "play-json-extra"            % "2.4.3",
     ws
   )
