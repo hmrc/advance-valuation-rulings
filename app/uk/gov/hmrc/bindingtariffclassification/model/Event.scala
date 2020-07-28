fd608ac56adac448d241c8393d66308779b048fd
Matthew Pomaroff <67267271+OC-Pomaroff@users.noreply.github.com>
2020-07-28 16:34:42 +0100

    DIT-1987 | Fixed typo in type for CompletedCaseStatusChange

--- app/uk/gov/hmrc/bindingtariffclassification/model/Event.scala
+++ app/uk/gov/hmrc/bindingtariffclassification/model/Event.scala
@@ -95,7 +95,7 @@ case class CompletedCaseStatusChange
   email: Option[String]
 ) extends FieldChange[CaseStatus] {
   override val to: CaseStatus = CaseStatus.COMPLETED
-  override val `type`: EventType.Value = EventType.CASE_REFERRAL
+  override val `type`: EventType.Value = EventType.CASE_COMPLETED
 }
 
 case class AppealAdded
