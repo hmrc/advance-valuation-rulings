097bb71a279b64afd06292e1df4f80231290c976
nboaram <14924378+Nboaram@users.noreply.github.com>
2022-07-07 11:42:06 +0100

    [DDCE-3097] Dependency Upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,14 +5,14 @@ import play.core.PlayVersion.current
 object AppDependencies {
 
   lazy val compile = Seq(
-    "uk.gov.hmrc"          %% "bootstrap-backend-play-28"     % "5.14.0",
-    "uk.gov.hmrc"          %% "crypto"                        % "6.0.0",
+    "uk.gov.hmrc"          %% "bootstrap-backend-play-28"     % "5.24.0",
+    "uk.gov.hmrc"          %% "crypto"                        % "6.1.0",
     "uk.gov.hmrc"          %% "play-json-union-formatter"     % "1.15.0-play-28",
-    "uk.gov.hmrc"          %% "simple-reactivemongo"          % "7.31.0-play-27",
-    "uk.gov.hmrc"          %% "mongo-lock"                    % "7.0.0-play-27",
+    "uk.gov.hmrc"          %% "simple-reactivemongo"          % "8.1.0-play-27",
+    "uk.gov.hmrc"          %% "mongo-lock"                    % "7.1.0-play-27",
     "org.reactivemongo"    %% "reactivemongo-akkastream"      % "0.18.8",
     "com.typesafe.play"    %% "play-json"                     % "2.9.2",
-    "org.typelevel"        %% "cats-core"                     % "2.6.1",
+    "org.typelevel"        %% "cats-core"                     % "2.8.0",
     "com.github.pathikrit" %% "better-files"                  % "3.9.1",
     "org.quartz-scheduler" % "quartz"                         % "2.3.2",
     ws
@@ -23,20 +23,19 @@ object AppDependencies {
   val test: Seq[ModuleID] = Seq(
     "com.github.tomakehurst" % "wiremock"                  % "2.27.2",
     "com.typesafe.play"      %% "play-test"                % current ,
-    "org.mockito"            % "mockito-core"              % "3.11.2",
-    "org.jsoup"              % "jsoup"                     % "1.14.2",
+    "org.mockito"            % "mockito-core"              % "4.6.1",
+    "org.jsoup"              % "jsoup"                     % "1.15.2",
     "org.pegdown"            % "pegdown"                   % "1.6.0" ,
     "org.scalatest"          %% "scalatest"                % "3.2.9" ,
     "org.scalatestplus.play" %% "scalatestplus-play"       % "5.1.0" ,
     "org.scalatestplus"      %% "mockito-3-4"              % "3.2.9.0",
     "com.vladsch.flexmark"   % "flexmark-all"              % "0.35.10",
-    "org.scalacheck"         %% "scalacheck"               % "1.15.4",
-//    "uk.gov.hmrc"            %% "http-verbs-test"          % "1.8.0-play-27",
+    "org.scalacheck"         %% "scalacheck"               % "1.16.0",
     "uk.gov.hmrc"            %% "http-verbs-play-28"       % "13.8.1-RC1",
-    "uk.gov.hmrc"            %% "service-integration-test" % "1.1.0-play-28",
-    "uk.gov.hmrc"            %% "reactivemongo-test"       % "4.22.0-play-27",
+    "uk.gov.hmrc"            %% "service-integration-test" % "1.3.0-play-28",
+    "uk.gov.hmrc"            %% "reactivemongo-test"       % "5.0.0-play-27",
     "org.scalaj"             %% "scalaj-http"              % "2.4.2",
-    //Need to peg this version for wiremock - try removing this on next lib upgrade
+
     "org.eclipse.jetty" % "jetty-server"  % jettyVersion,
     "org.eclipse.jetty" % "jetty-servlet" % jettyVersion
   ).map(_ % "test, it")
