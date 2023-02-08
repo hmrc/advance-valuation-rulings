f1fc840c6c2c1657bd15afcd7e8ec29f3cdda90b
koladeadewuyi <koladeadewuyi@users.noreply.github.com>
2023-02-08 11:57:52 +0000

    ARS-483: Add ETMP trader details models (#1)

---
+++ app/uk/gov/hmrc/advancevaluationrulings/models/errors/ErrorDetail.scala
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.advancevaluationrulings.models.errors
+
+import java.time.LocalDateTime
+
+final case class ErrorDetail(
+  timestamp: LocalDateTime,
+  correlationId: String,
+  errorCode: String,
+  source: String,
+  errorMessage: Option[String] = None,
+  sourceFaultDetail: Option[SourceFaultDetail] = None
+)
