12e671b464f370f892dfe074aa26feba10e17532
Kolade Adewuyi <koladeadewuyi@users.noreply.github.com>
2023-02-15 16:07:02 +0000

    ARS-483: Implement controller logic

--- app/uk/gov/hmrc/advancevaluationrulings/controllers/ARSController.scala
+++ app/uk/gov/hmrc/advancevaluationrulings/controllers/ARSController.scala
@@ -1,20 +1,19 @@
 package uk.gov.hmrc.advancevaluationrulings.controllers
 
-import javax.inject.{Inject, Singleton}
-
-import scala.concurrent.ExecutionContext
-
 import play.api.libs.json.JsValue
 import play.api.mvc.{Action, ControllerComponents}
 import uk.gov.hmrc.advancevaluationrulings.models.traderdetails.{TraderDetailsRequest, TraderDetailsResponse}
 import uk.gov.hmrc.advancevaluationrulings.services.TraderDetailsService
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.ExecutionContext
+
 @Singleton()
 class ARSController @Inject() (
   cc: ControllerComponents,
   traderDetailsService: TraderDetailsService
-) extends BackendController(cc) {
+) extends BackendController(cc) with BaseController {
 
   implicit val ec: ExecutionContext = cc.executionContext
 
@@ -22,6 +21,15 @@ class ARSController @Inject() (
     // convert request from Json to TraderDetailsRequest
     // authenticate ? maybe
     // call service with TraderDetailsRequest and return TraderDetailsResponse
+
+    Action.async(parse.json) { implicit request =>
+      withCustomJsonBody[TraderDetailsRequest] { traderDetailsRequest =>
+        inHttpResponse[TraderDetailsResponse](200) {
+          import traderDetailsRequest._
+          traderDetailsService.getTraderDetails(regime, acknowledgementReference, taxPayerID, EORI)
+        }
+      }
+    }
   }
 
 }
---
+++ app/uk/gov/hmrc/advancevaluationrulings/controllers/BaseController.scala
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ */
+
+package uk.gov.hmrc.advancevaluationrulings.controllers
+
+import play.api.libs.json._
+import play.api.mvc.{Request, Result}
+import uk.gov.hmrc.advancevaluationrulings.models.common.Envelope.Envelope
+import uk.gov.hmrc.http.HeaderCarrier
+
+import scala.concurrent.{ExecutionContext, Future}
+
+trait BaseController {
+  def inHttpResponse[T](successStatusCode: Int)(result: => Envelope[T])(
+      implicit ec: ExecutionContext,
+      hc:          HeaderCarrier,
+      format:      Format[T]
+    ): Future[Result] = ???
+
+  def withCustomJsonBody[T](f: T => Future[Result])(
+      implicit request: Request[JsValue],
+      reads:            Reads[T]
+    ): Future[Result] = ???
+}
