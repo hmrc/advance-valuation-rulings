838e29518c3afad2510573981351a6149600cf5d
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2023-03-29 18:07:26 +0100

    Add repo to provide unique ids

---
+++ app/uk/gov/hmrc/advancevaluationrulings/repositories/CounterRepository.scala
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.advancevaluationrulings.repositories
+
+import org.mongodb.scala.MongoBulkWriteException
+import org.mongodb.scala.model.{Filters, FindOneAndUpdateOptions, ReturnDocument, Updates}
+import uk.gov.hmrc.advancevaluationrulings.models.Done
+import uk.gov.hmrc.advancevaluationrulings.models.application.{CounterId, CounterWrapper}
+import uk.gov.hmrc.mongo.MongoComponent
+import uk.gov.hmrc.mongo.play.json.PlayMongoRepository
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.{ExecutionContext, Future}
+import scala.jdk.CollectionConverters._
+
+@Singleton
+class CounterRepository @Inject()(
+                                   mongoComponent: MongoComponent
+                                 )(implicit ec: ExecutionContext)
+  extends PlayMongoRepository[CounterWrapper](
+    collectionName = "ids",
+    mongoComponent = mongoComponent,
+    domainFormat   = CounterWrapper.format,
+    indexes        = Nil
+  ) {
+
+  private val duplicateErrorCode = 11000
+  private def byId(id: CounterId) = Filters.eq("_id", id.toString)
+
+  override lazy val requiresTtlIndex: Boolean = false
+
+  private[repositories] val seeds: Seq[CounterWrapper] = Seq(
+    CounterWrapper(CounterId.ApplicationId, 0),
+    CounterWrapper(CounterId.AttachmentId, 0)
+  )
+
+  private val seedDatabase = seeds // Eagerly call seed to ensure records are created on startup if needed
+
+  def seed: Future[Done] =
+    collection.insertMany(seeds)
+      .toFuture()
+      .map(_ => Done)
+      .recover {
+        case e: MongoBulkWriteException if e.getWriteErrors.asScala.forall(x => x.getCode == duplicateErrorCode) => Done
+      }
+
+  def nextId(id: CounterId): Future[Long] =
+    collection
+      .findOneAndUpdate(
+        filter = byId(id),
+        update = Updates.inc("index", 1),
+        options = FindOneAndUpdateOptions()
+          .upsert(true)
+          .bypassDocumentValidation(false)
+          .returnDocument(ReturnDocument.AFTER)
+      )
+      .toFuture
+      .map(_.index)
+}
---
+++ it/uk/gov/hmrc/advancevaluationrulings/repositories/CounterRepositorySpec.scala
@@ -0,0 +1,58 @@
+package uk.gov.hmrc.advancevaluationrulings.repositories
+
+import org.scalatest.OptionValues
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.must.Matchers
+import uk.gov.hmrc.advancevaluationrulings.models.application.{CounterId, CounterWrapper}
+import uk.gov.hmrc.mongo.test.DefaultPlayMongoRepositorySupport
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class CounterRepositorySpec
+  extends AnyFreeSpec
+    with Matchers
+    with DefaultPlayMongoRepositorySupport[CounterWrapper]
+    with OptionValues
+    with ScalaFutures {
+
+  protected override val repository = new CounterRepository(mongoComponent)
+
+  "on startup" - {
+
+    "must insert seed records when they do not already exist" in {
+
+      repository.seed.futureValue
+
+      findAll().futureValue must contain theSameElementsAs repository.seeds
+    }
+  }
+
+  ".seed" - {
+
+    "must not fail when records already exist" in {
+
+      repository.seed.futureValue
+      repository.seed.futureValue
+
+      findAll().futureValue must contain theSameElementsAs repository.seeds
+    }
+  }
+
+  ".nextId" - {
+
+    "must return sequential ids from the correct record" in {
+
+      val applicationIdStartingValue = repository.seeds.find(_._id == CounterId.ApplicationId).head.index
+      val attachmentIdStartingValue = repository.seeds.find(_._id == CounterId.AttachmentId).head.index
+
+      repository.nextId(CounterId.ApplicationId).futureValue mustEqual applicationIdStartingValue + 1
+      repository.nextId(CounterId.ApplicationId).futureValue mustEqual applicationIdStartingValue + 2
+      repository.nextId(CounterId.ApplicationId).futureValue mustEqual applicationIdStartingValue + 3
+
+      repository.nextId(CounterId.AttachmentId).futureValue mustEqual attachmentIdStartingValue + 1
+      repository.nextId(CounterId.AttachmentId).futureValue mustEqual attachmentIdStartingValue + 2
+      repository.nextId(CounterId.AttachmentId).futureValue mustEqual attachmentIdStartingValue + 3
+    }
+  }
+}
