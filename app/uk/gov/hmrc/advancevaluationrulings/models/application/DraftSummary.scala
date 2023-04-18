b79fee5f808ef8a01e73bae6a91b13cc5cd3f534
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2023-04-18 16:46:12 +0100

    Add DraftSummary model

---
+++ app/uk/gov/hmrc/advancevaluationrulings/models/application/DraftSummary.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.advancevaluationrulings.models.application
+
+import play.api.libs.json._
+import uk.gov.hmrc.advancevaluationrulings.models.{DraftId, UserAnswers}
+
+import java.time.Instant
+
+final case class DraftSummary(
+                               id: DraftId,
+                               goodsName: Option[String],
+                               lastUpdated: Instant,
+                               eoriNumber: Option[String]
+                             )
+
+object DraftSummary {
+
+  implicit lazy val format: OFormat[DraftSummary] = Json.format
+
+  def apply(answers: UserAnswers): DraftSummary =
+    DraftSummary(
+      id = answers.draftId,
+      goodsName = Reads.optionNoError(Reads.at[String](JsPath \ "goodsDescription")).reads(answers.data).getOrElse(None),
+      lastUpdated = answers.lastUpdated,
+      eoriNumber = Reads.optionNoError(Reads.at[String](JsPath \ "checkRegisteredDetails" \ "eori")).reads(answers.data).getOrElse(None)
+    )
+}
---
+++ test/uk/gov/hmrc/advancevaluationrulings/models/application/DraftSummarySpec.scala
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.advancevaluationrulings.models.application
+
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.must.Matchers
+import play.api.libs.json.{JsSuccess, Json}
+import uk.gov.hmrc.advancevaluationrulings.models.{DraftId, UserAnswers}
+
+import java.time.Instant
+
+class DraftSummarySpec extends AnyFreeSpec with Matchers {
+
+  private val now = Instant.now
+
+  ".apply" - {
+
+    "must return a DraftSummary when optional fields have not been answered" in {
+
+      val draftId = DraftId(0)
+      val answers = UserAnswers("userId", draftId, Json.obj(), now)
+
+      DraftSummary(answers) mustEqual DraftSummary(draftId, None, now, None)
+    }
+
+    "must return a DraftSummary when optional fields have been answered" in {
+
+      val draftId = DraftId(0)
+      val data = Json.obj(
+        "goodsDescription" -> "goods",
+        "checkRegisteredDetails" -> Json.obj(
+          "eori" -> "eori"
+        )
+      )
+
+      val answers = UserAnswers("userId", draftId, data, now)
+
+      DraftSummary(answers) mustEqual DraftSummary(draftId, Some("goods"), now, Some("eori"))
+    }
+  }
+}
