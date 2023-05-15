8629778f4e2b1776c24fc5126a9d111b4d574907
Amaal Ali <9220299+amaalali@users.noreply.github.com>
2023-05-15 16:53:21 +0100

    ARS-865 Add confidential prefix for confidential files

--- it/uk/gov/hmrc/advancevaluationrulings/connectors/DmsSubmissionConnectorSpec.scala
+++ it/uk/gov/hmrc/advancevaluationrulings/connectors/DmsSubmissionConnectorSpec.scala
@@ -57,6 +57,7 @@ class DmsSubmissionConnectorSpec
     super.beforeEach()
     resetWireMock()
     objectStoreStub.deleteObject(Path.File("foo/bar.pdf")).futureValue
+    objectStoreStub.deleteObject(Path.File("foo/baz.pdf")).futureValue
   }
 
   implicit private lazy val hc: HeaderCarrier = HeaderCarrier()
@@ -111,68 +112,155 @@ class DmsSubmissionConnectorSpec
       size = 1337
     )
 
-    "must return Done when the server returns ACCEPTED" in {
+    "must return Done when the server returns ACCEPTED" - {
+      "when the file is Public" in {
+        objectStoreStub
+          .putObject(
+            path = Path.File("foo/bar.pdf"),
+            content = Source.single(ByteString.fromString("Attachment 1")),
+            contentType = Some("application/pdf")
+          )
+          .futureValue
 
-      objectStoreStub
-        .putObject(
-          path = Path.File("foo/bar.pdf"),
-          content = Source.single(ByteString.fromString("Attachment 1")),
-          contentType = Some("application/pdf")
+        wireMockServer.stubFor(
+          post(urlEqualTo("/dms-submission/submit"))
+            .withHeader(AUTHORIZATION, equalTo("authKey"))
+            .withHeader(USER_AGENT, equalTo("advance-valuation-rulings"))
+            .withMultipartRequestBody(
+              aMultipart().withName("submissionReference").withBody(equalTo("submissionReference"))
+            )
+            .withMultipartRequestBody(
+              aMultipart().withName("callbackUrl").withBody(equalTo("http://localhost/callback"))
+            )
+            .withMultipartRequestBody(
+              aMultipart().withName("metadata.source").withBody(equalTo("advance-ruling-service"))
+            )
+            .withMultipartRequestBody(
+              aMultipart()
+                .withName("metadata.timeOfReceipt")
+                .withBody(equalTo("2022-03-02T12:30:45"))
+            )
+            .withMultipartRequestBody(
+              aMultipart().withName("metadata.formId").withBody(equalTo("formId"))
+            )
+            .withMultipartRequestBody(
+              aMultipart().withName("metadata.customerId").withBody(equalTo("someEori"))
+            )
+            .withMultipartRequestBody(
+              aMultipart()
+                .withName("metadata.classificationType")
+                .withBody(equalTo("classificationType"))
+            )
+            .withMultipartRequestBody(
+              aMultipart().withName("metadata.businessArea").withBody(equalTo("businessArea"))
+            )
+            .withMultipartRequestBody(
+              aMultipart()
+                .withName("form")
+                .withBody(equalTo("Hello, World!"))
+            )
+            .withMultipartRequestBody(
+              aMultipart()
+                .withName("attachment")
+                .withBody(equalTo("Attachment 1"))
+                .withHeader("Content-Disposition", containing("""filename="bar.pdf""""))
+                .withHeader("Content-Type", equalTo("application/pdf"))
+            )
+            .willReturn(
+              aResponse()
+                .withStatus(ACCEPTED)
+                .withBody(Json.stringify(Json.obj("id" -> "foobar")))
+            )
         )
-        .futureValue
 
-      wireMockServer.stubFor(
-        post(urlEqualTo("/dms-submission/submit"))
-          .withHeader(AUTHORIZATION, equalTo("authKey"))
-          .withHeader(USER_AGENT, equalTo("advance-valuation-rulings"))
-          .withMultipartRequestBody(
-            aMultipart().withName("submissionReference").withBody(equalTo("submissionReference"))
-          )
-          .withMultipartRequestBody(
-            aMultipart().withName("callbackUrl").withBody(equalTo("http://localhost/callback"))
-          )
-          .withMultipartRequestBody(
-            aMultipart().withName("metadata.source").withBody(equalTo("advance-ruling-service"))
-          )
-          .withMultipartRequestBody(
-            aMultipart().withName("metadata.timeOfReceipt").withBody(equalTo("2022-03-02T12:30:45"))
-          )
-          .withMultipartRequestBody(
-            aMultipart().withName("metadata.formId").withBody(equalTo("formId"))
-          )
-          .withMultipartRequestBody(
-            aMultipart().withName("metadata.customerId").withBody(equalTo("someEori"))
-          )
-          .withMultipartRequestBody(
-            aMultipart()
-              .withName("metadata.classificationType")
-              .withBody(equalTo("classificationType"))
-          )
-          .withMultipartRequestBody(
-            aMultipart().withName("metadata.businessArea").withBody(equalTo("businessArea"))
-          )
-          .withMultipartRequestBody(
-            aMultipart()
-              .withName("form")
-              .withBody(equalTo("Hello, World!"))
-          )
-          .withMultipartRequestBody(
-            aMultipart()
-              .withName("attachment")
-              .withBody(equalTo("Attachment 1"))
-              .withHeader("Content-Disposition", containing("""filename="bar.pdf""""))
-              .withHeader("Content-Type", equalTo("application/pdf"))
-          )
-          .willReturn(
-            aResponse()
-              .withStatus(ACCEPTED)
-              .withBody(Json.stringify(Json.obj("id" -> "foobar")))
-          )
-      )
+        connector
+          .submitApplication(eori, source, timestamp, submissionReference, Seq(attachment))(hc)
+          .futureValue
+      }
 
-      connector
-        .submitApplication(eori, source, timestamp, submissionReference, Seq(attachment))(hc)
-        .futureValue
+      "when the file is Confidential" in {
+        val confidentialAttachment = Attachment(
+          id = 2,
+          name = "attachment2",
+          description = None,
+          location = "foo/baz.pdf",
+          privacy = Privacy.Confidential,
+          mimeType = "application/pdf",
+          size = 1337
+        )
+
+        objectStoreStub
+          .putObject(
+            path = Path.File(confidentialAttachment.location),
+            content = Source.single(ByteString.fromString("Attachment 2")),
+            contentType = Some("application/pdf")
+          )
+          .futureValue
+
+        wireMockServer.stubFor(
+          post(urlEqualTo("/dms-submission/submit"))
+            .withHeader(AUTHORIZATION, equalTo("authKey"))
+            .withHeader(USER_AGENT, equalTo("advance-valuation-rulings"))
+            .withMultipartRequestBody(
+              aMultipart().withName("submissionReference").withBody(equalTo("submissionReference"))
+            )
+            .withMultipartRequestBody(
+              aMultipart().withName("callbackUrl").withBody(equalTo("http://localhost/callback"))
+            )
+            .withMultipartRequestBody(
+              aMultipart().withName("metadata.source").withBody(equalTo("advance-ruling-service"))
+            )
+            .withMultipartRequestBody(
+              aMultipart()
+                .withName("metadata.timeOfReceipt")
+                .withBody(equalTo("2022-03-02T12:30:45"))
+            )
+            .withMultipartRequestBody(
+              aMultipart().withName("metadata.formId").withBody(equalTo("formId"))
+            )
+            .withMultipartRequestBody(
+              aMultipart().withName("metadata.customerId").withBody(equalTo("someEori"))
+            )
+            .withMultipartRequestBody(
+              aMultipart()
+                .withName("metadata.classificationType")
+                .withBody(equalTo("classificationType"))
+            )
+            .withMultipartRequestBody(
+              aMultipart().withName("metadata.businessArea").withBody(equalTo("businessArea"))
+            )
+            .withMultipartRequestBody(
+              aMultipart()
+                .withName("form")
+                .withBody(equalTo("Hello, World!"))
+            )
+            .withMultipartRequestBody(
+              aMultipart()
+                .withName("attachment")
+                .withBody(equalTo("Attachment 2"))
+                .withHeader(
+                  "Content-Disposition",
+                  containing("""filename="CONFIDENTIAL_baz.pdf"""")
+                )
+                .withHeader("Content-Type", equalTo("application/pdf"))
+            )
+            .willReturn(
+              aResponse()
+                .withStatus(ACCEPTED)
+                .withBody(Json.stringify(Json.obj("id" -> "foobar")))
+            )
+        )
+
+        connector
+          .submitApplication(
+            eori,
+            source,
+            timestamp,
+            submissionReference,
+            Seq(confidentialAttachment)
+          )(hc)
+          .futureValue
+      }
     }
 
     "must fail when an attachment cannot be found in object-store" in {
