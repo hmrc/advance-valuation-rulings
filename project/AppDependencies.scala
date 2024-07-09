b00dd8f96e6ebc55058d72665331927c33bab1f5
lmugnai <11977337+lmugnai@users.noreply.github.com>
2024-07-09 16:19:26 +0100

    DDCE-5403: remove platform-url + dep updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,24 +2,24 @@ import sbt.*
 
 object AppDependencies {
 
-  private lazy val hmrcBootstrapPlayVersion = "8.6.0"
-  private lazy val hmrcMongoPlayVersion     = "1.9.0"
+  private lazy val hmrcBootstrapPlayVersion = "9.0.0"
+  private lazy val hmrcMongoPlayVersion     = "2.1.0"
 
   private val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"    % hmrcBootstrapPlayVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"           % hmrcMongoPlayVersion,
-    "com.beachape"            %% "enumeratum-play-json"         % "1.8.0",
+    "com.beachape"            %% "enumeratum-play-json"         % "1.8.1",
     "org.typelevel"           %% "cats-core"                    % "2.12.0",
     "org.apache.xmlgraphics"   % "fop"                          % "2.9",
     "uk.gov.hmrc"             %% "crypto-json-play-30"          % "8.0.0",
-    "uk.gov.hmrc"             %% "internal-auth-client-play-30" % "2.0.0",
-    "uk.gov.hmrc.objectstore" %% "object-store-client-play-30"  % "1.4.0"
+    "uk.gov.hmrc"             %% "internal-auth-client-play-30" % "3.0.0",
+    "uk.gov.hmrc.objectstore" %% "object-store-client-play-30"  % "2.0.0"
   )
 
   private val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % hmrcBootstrapPlayVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoPlayVersion,
-    "org.mockito"       %% "mockito-scala"           % "1.17.31",
+    "org.mockito"       %% "mockito-scala"           % "1.17.37",
     "wolfendale"        %% "scalacheck-gen-regexp"   % "0.1.2",
     "org.scalatestplus" %% "scalacheck-1-17"         % "3.2.18.0",
     "org.apache.pdfbox"  % "pdfbox"                  % "3.0.2"
